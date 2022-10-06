c2d6398ccda0a113c6bcc9652c864f4b95311521
Ruslan <Ruslan@.ru>
2022-10-06 20:56:11 +0500

    final

---
+++ consol.js
@@ -0,0 +1,9 @@
+var x = +prompt ('Введите Первое число')
+var y = +prompt ('Введите второе число')
+var z = +prompt ('Введите Третье число')
+var res = (x + y + z) / 3
+alert( res )
+console.log( x );
+console.log( y );
+console.log( z );
+console.log( res );
